a701046e4588a4a824ebe50267463ee7e9400f5e
Vy Nghia <dora.nghia@gmail.com>
2019-02-10 12:58:48 +0700

    Update admin_options.php

--- system/data/admin_options.php
+++ system/data/admin_options.php
@@ -7,8 +7,8 @@ if(isset($_SESSION["admin"]) && $Level == 1)
 {
 		if($_GET["opt"] == "google_short_link" && $_POST["v"] == 0 ||  $_POST["v"] == 1)
 		{
-			mysqli_query($db, "UPDATE `options` SET `google_short_link`='{$_POST["v"]}' WHERE 1");
+			mysqli_query($con, "UPDATE `options` SET `google_short_link`='{$_POST["v"]}' WHERE 1");
 		}
 		
-	mysqli_close($db);
+	mysqli_close($con);
 }
