1d7cb0ca02fc0cfe08f0d3b60400ed0dd4dcdfca
Vy Nghia <dora.nghia@gmail.com>
2019-02-10 12:33:00 +0700

    fix login

--- system/data/admin_login.php
+++ system/data/admin_login.php
@@ -5,13 +5,13 @@ require '../config.php';
 
 if(isset($_POST['username']) && isset($_POST['password'])){
 	$checkUser = mysqli_query($con, "SELECT * FROM `manager` WHERE `username` = '{$_POST['username']}'");
-	if(mysqli_fetch_array($checkUser) === false)
+	if(mysqli_num_rows($checkUser) < 1) {
 		echo (2);
-		else {
-			$checkUserPass = mysqli_query($con, "SELECT * FROM `manager` WHERE `username` = '{$_POST['username']}' AND `password` = '{$_POST['password']}'");
-		if(mysqli_fetch_array($checkUserPass) === false)
+	} else {
+		$checkUserPass = mysqli_query($con, "SELECT * FROM `manager` WHERE `username` = '{$_POST['username']}' AND `password` = '{$_POST['password']}'");
+		if(mysqli_num_rows($checkUserPass) < 1){
 			echo (3);
-		else {
+		} else {
 			echo (1);
 			$_SESSION['admin'] = $_POST['username'];
 		}
