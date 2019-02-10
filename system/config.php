d04d2673aa4f497a3b43ed8ab57f3860eeed9325
Vy Nghia <dora.nghia@gmail.com>
2019-02-10 11:18:35 +0700

    New updates (mysql, var,...)

--- system/config.php
+++ system/config.php
@@ -1,15 +1,15 @@
 <?php
 /* >_ Developed by Vy Nghia */
 require 'class/ProtectClass.php';
-define('WEBURL', 'https://nghia.org');
+define('WEBURL', 'https://cloud.nghia.org');
 
 $db = new Database;
 $db->dbhost('localhost');
-$db->dbuser('username');
-$db->dbpass('password');
-$db->dbname('db_name');
+$db->dbuser('ctjoppmqhosting_vnghia');
+$db->dbpass('1151985611');
+$db->dbname('ctjoppmqhosting_nghia');
 
-$db->connect(); // mysqli var is $db
+$con = $db->connect(); // mysqli var is $con
 
 include_once 'content.php';
 include_once 'data/admin_api.php';
